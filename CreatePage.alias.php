62f1c3e354e7c128633f0bc61ca8196d52377fc7
Jack Phoenix <ashley@uncyclomedia.co>
2019-12-15 15:04:36 +0200

    Add special page alias file to make special page name translatable
    Extension setup file (CreateAPage.php) intentionally *not* updated in this commit and will be done in a future commit to keep the diffs even remotely readable
    
    Change-Id: I94a6f7ac6d5679ead535be9cc74f28ca4475321f

---
+++ CreatePage.alias.php
@@ -0,0 +1,339 @@
+<?php
+/**
+ * Aliases for Special:CreatePage
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English */
+$specialPageAliases['en'] = [
+	'CreatePage' => [ 'CreatePage' ],
+];
+
+/** Afrikaans (Afrikaans) */
+$specialPageAliases['af'] = [
+	'CreatePage' => [ 'SkepBladsy' ],
+];
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = [
+	'CreatePage' => [ 'إنشاء_صفحة' ],
+];
+
+/** Aramaic (ܐܪܡܝܐ) */
+$specialPageAliases['arc'] = [
+	'CreatePage' => [ 'ܒܪܝ_ܦܐܬܐ' ],
+];
+
+/** Egyptian Spoken Arabic (مصرى) */
+$specialPageAliases['arz'] = [
+	'CreatePage' => [ 'اعمل_صفحه' ],
+];
+
+/** Assamese (অসমীয়া) */
+$specialPageAliases['as'] = [
+	'CreatePage' => [ 'পৃষ্ঠা_সৃষ্টি' ],
+];
+
+/** Azerbaijani (azərbaycanca) */
+$specialPageAliases['az'] = [
+	'CreatePage' => [ 'SəhifəYarat' ],
+];
+
+/** Bulgarian (български) */
+$specialPageAliases['bg'] = [
+	'CreatePage' => [ 'Създаване_на_страница' ],
+];
+
+/** Banjar (Bahasa Banjar) */
+$specialPageAliases['bjn'] = [
+	'CreatePage' => [ 'Ulah_tungkaran' ],
+];
+
+/** Breton (brezhoneg) */
+$specialPageAliases['br'] = [
+	'CreatePage' => [ 'KrouiñPajenn' ],
+];
+
+/** Bosnian (bosanski) */
+$specialPageAliases['bs'] = [
+	'CreatePage' => [ 'NapraviStranicu' ],
+];
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = [
+	'CreatePage' => [ 'Seite_erstellen' ],
+];
+
+/** Zazaki (Zazaki) */
+$specialPageAliases['diq'] = [
+	'CreatePage' => [ 'PelaVıraze' ],
+];
+
+/** Lower Sorbian (dolnoserbski) */
+$specialPageAliases['dsb'] = [
+	'CreatePage' => [ 'Bok napóraś' ],
+];
+
+/** Esperanto (Esperanto) */
+$specialPageAliases['eo'] = [
+	'CreatePage' => [ 'Krei_paĝon' ],
+];
+
+/** Spanish (español) */
+$specialPageAliases['es'] = [
+	'CreatePage' => [ 'Crear_página', 'CrearPágina' ],
+];
+
+/** Persian (فارسی) */
+$specialPageAliases['fa'] = [
+	'CreatePage' => [ 'ایجاد_صفحه' ],
+];
+
+/** Finnish (suomi) */
+$specialPageAliases['fi'] = [
+	'CreatePage' => [ 'Luo_sivu', 'LuoSivu' ],
+];
+
+/** Franco-Provençal (arpetan) */
+$specialPageAliases['frp'] = [
+	'CreatePage' => [ 'Fâre_una_pâge', 'FâreUnaPâge' ],
+];
+
+/** Gagauz (Gagauz) */
+$specialPageAliases['gag'] = [
+	'CreatePage' => [ 'SayfaYarat', 'YazıYarat', 'YaprakYarat' ],
+];
+
+/** Galician (galego) */
+$specialPageAliases['gl'] = [
+	'CreatePage' => [ 'Crear_a_páxina' ],
+];
+
+/** Swiss German (Alemannisch) */
+$specialPageAliases['gsw'] = [
+	'CreatePage' => [ 'Syte_aalege' ],
+];
+
+/** Croatian (hrvatski) */
+$specialPageAliases['hr'] = [
+	'CreatePage' => [ 'Započni_stranicu' ],
+];
+
+/** Upper Sorbian (hornjoserbsce) */
+$specialPageAliases['hsb'] = [
+	'CreatePage' => [ 'Stronu wutworić' ],
+];
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = [
+	'CreatePage' => [ 'KreyePaj' ],
+];
+
+/** Hungarian (magyar) */
+$specialPageAliases['hu'] = [
+	'CreatePage' => [ 'Oldalkészítés', 'Oldal_készítése' ],
+];
+
+/** Interlingua (interlingua) */
+$specialPageAliases['ia'] = [
+	'CreatePage' => [ 'Crear_pagina' ],
+];
+
+/** Indonesian (Bahasa Indonesia) */
+$specialPageAliases['id'] = [
+	'CreatePage' => [ 'Buat_halaman', 'BuatHalaman' ],
+];
+
+/** Italian (italiano) */
+$specialPageAliases['it'] = [
+	'CreatePage' => [ 'CreaPagina' ],
+];
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = [
+	'CreatePage' => [ 'ページ作成', 'ページの作成' ],
+];
+
+/** Georgian (ქართული) */
+$specialPageAliases['ka'] = [
+	'CreatePage' => [ 'გვერდის_შექმნა' ],
+];
+
+/** Khmer (ភាសាខ្មែរ) */
+$specialPageAliases['km'] = [
+	'CreatePage' => [ 'បង្កើត​ទំព័រ' ],
+];
+
+/** Korean (한국어) */
+$specialPageAliases['ko'] = [
+	'CreatePage' => [ '문서만들기' ],
+];
+
+/** Colognian (Ripoarisch) */
+$specialPageAliases['ksh'] = [
+	'CreatePage' => [ 'Sigge_aanlääje', 'Sigg_aanlääje' ],
+];
+
+/** Cornish (kernowek) */
+$specialPageAliases['kw'] = [
+	'CreatePage' => [ 'GwruthylFolen' ],
+];
+
+/** Ladino (Ladino) */
+$specialPageAliases['lad'] = [
+	'CreatePage' => [ 'Criar_la_hoja' ],
+];
+
+/** Luxembourgish (Lëtzebuergesch) */
+$specialPageAliases['lb'] = [
+	'CreatePage' => [ 'Säiten_uleeën' ],
+];
+
+/** lumbaart (lumbaart) */
+$specialPageAliases['lmo'] = [
+	'CreatePage' => [ 'FaSüPagina' ],
+];
+
+/** Lithuanian (lietuvių) */
+$specialPageAliases['lt'] = [
+	'CreatePage' => [ 'Kurti puslapį' ],
+];
+
+/** Macedonian (македонски) */
+$specialPageAliases['mk'] = [
+	'CreatePage' => [ 'СоздајСтраница' ],
+];
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = [
+	'CreatePage' => [ 'താളുണ്ടാക്കുക' ],
+];
+
+/** Marathi (मराठी) */
+$specialPageAliases['mr'] = [
+	'CreatePage' => [ 'पृष्ठनिर्मिसी' ],
+];
+
+/** Maltese (Malti) */
+$specialPageAliases['mt'] = [
+	'CreatePage' => [ 'OħloqPaġna' ],
+];
+
+/** Norwegian Bokmål (norsk (bokmål)) */
+$specialPageAliases['nb'] = [
+	'CreatePage' => [ 'Opprett_side' ],
+];
+
+/** Nedersaksisch (Nedersaksisch) */
+$specialPageAliases['nds-nl'] = [
+	'CreatePage' => [ 'Pagina_anmaken' ],
+];
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = [
+	'CreatePage' => [ 'PaginaAanmaken' ],
+];
+
+/** Polish (polski) */
+$specialPageAliases['pl'] = [
+	'CreatePage' => [ 'UtwórzStronę' ],
+];
+
+/** Pashto (پښتو) */
+$specialPageAliases['ps'] = [
+	'CreatePage' => [ 'مخ جوړول', 'CreatPage' ],
+];
+
+/** Portuguese (português) */
+$specialPageAliases['pt'] = [
+	'CreatePage' => [ 'Criar_página' ],
+];
+
+/** Sanskrit (संस्कृतम्) */
+$specialPageAliases['sa'] = [
+	'CreatePage' => [ 'पृष्ठस्यसृजन' ],
+];
+
+/** Slovak (slovenčina) */
+$specialPageAliases['sk'] = [
+	'CreatePage' => [ 'VytvoriťStránku' ],
+];
+
+/** Serbian (Cyrillic script) (српски (ћирилица)‎) */
+$specialPageAliases['sr-ec'] = [
+	'CreatePage' => [ 'Направи_страницу' ],
+];
+
+/** Serbian (Latin script) (srpski (latinica)‎) */
+$specialPageAliases['sr-el'] = [
+	'CreatePage' => [ 'Napravi_stranicu' ],
+];
+
+/** Sundanese (Basa Sunda) */
+$specialPageAliases['su'] = [
+	'CreatePage' => [ 'JieunKaca' ],
+];
+
+/** Swedish (svenska) */
+$specialPageAliases['sv'] = [
+	'CreatePage' => [ 'Skapa_sida' ],
+];
+
+/** Swahili (Kiswahili) */
+$specialPageAliases['sw'] = [
+	'CreatePage' => [ 'AnzishaUkurasa' ],
+];
+
+/** Telugu (తెలుగు) */
+$specialPageAliases['te'] = [
+	'CreatePage' => [ 'పేజిసృష్టించు' ],
+];
+
+/** Thai (ไทย) */
+$specialPageAliases['th'] = [
+	'CreatePage' => [ 'สร้างหน้า' ],
+];
+
+/** Tagalog (Tagalog) */
+$specialPageAliases['tl'] = [
+	'CreatePage' => [ 'Likhain_ang_pahina', 'LikhaPahina' ],
+];
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = [
+	'CreatePage' => [ 'SayfaOluştur' ],
+];
+
+/** Tatar (Cyrillic script) (татарча) */
+$specialPageAliases['tt-cyrl'] = [
+	'CreatePage' => [ 'Битне_үзгәртү' ],
+];
+
+/** Ukrainian (українська) */
+$specialPageAliases['uk'] = [
+	'CreatePage' => [ 'Створити_сторінку' ],
+];
+
+/** vèneto (vèneto) */
+$specialPageAliases['vec'] = [
+	'CreatePage' => [ 'CreaPagina' ],
+];
+
+/** Vietnamese (Tiếng Việt) */
+$specialPageAliases['vi'] = [
+	'CreatePage' => [ 'Tạo_trang' ],
+];
+
+/** Simplified Chinese (中文（简体）‎) */
+$specialPageAliases['zh-hans'] = [
+	'CreatePage' => [ '创建页面' ],
+];
+
+/** Traditional Chinese (中文（繁體）‎) */
+$specialPageAliases['zh-hant'] = [
+	'CreatePage' => [ '創建頁面' ],
+];
