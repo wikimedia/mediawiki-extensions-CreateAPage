cbb214bb97726a220fbb857d3d9b2508eadf4897
Zoranzoki21 <zorandori4444@gmail.com>
2019-12-20 02:34:18 +0100

    build: Install npm linting for JS, JSON, i18n, and CSS files
    I manually fixed the css file as for it wasn't much errors/warnings.
    
    JS files disabled for now.
    
    Change-Id: Ib1d7853ecb638752424424ae23a792d172410144

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/* eslint-env node */
+
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		banana: {
+			all: 'i18n/',
+			options: {
+				requireLowerCase: 'initial'
+			}
+		},
+		eslint: {
+			options: {
+				extensions: [ '.js', '.json' ]
+			},
+			all: [
+				'**/*.{js,json}',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'**/*.css',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
