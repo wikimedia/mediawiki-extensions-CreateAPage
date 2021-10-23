wikimedia/mediawiki-extensions-CreateAPage

commit 6cf195214ea2287b30efa60b9a2e12def29a17c2

Fix strpos check against true
strpos can return false or an int. A strict check against true is not
needed.
Assume that a false is needed here.

Change-Id: I66bad5f4b79be10945939f617e6994d5d9327e4d

includes/CreateMultiPage.php
@@ -54,7 +54,7 @@ class CreateMultiPage {
 		if ( empty( $wgMultiEditTag ) || ( strpos( $sourceText, $multiedit_tag ) === false ) ) {
 			// @phan-suppress-previous-line PhanTypeMismatchArgumentNullableInternal phan doesn't like how $sourceText can (in theory, but not in practise) be empty
 			// @phan-suppress-next-line PhanTypeMismatchArgumentNullableInternal ditto
-			if ( strpos( $sourceText, self::ISBLANK_TAG_SPECIFIC ) !== true ) {
+			if ( strpos( $sourceText, self::ISBLANK_TAG_SPECIFIC ) === false ) {
 				// @phan-suppress-next-line PhanTypeMismatchArgumentNullableInternal
 				$sourceText = str_replace( self::ISBLANK_TAG_SPECIFIC . "\n", '', $sourceText );
 				$sourceText = str_replace( self::ISBLANK_TAG_SPECIFIC, '', $sourceText );

commit 6f5a11422057c1c13ec279cde5ab8a78a0d2d696

Change condition to unbreak the displaying of the main textarea on Special:CreatePage for a nonexistent createplate
Follow-up to 6cf195214ea2287b30efa60b9a2e12def29a17c2.

Assuming [[MediaWiki:Createplate-list]] contains the following:

character|Character
actor|Actor
Blank|Blank

but [[MediaWiki:Createplate-character]] does not exist, Special:CreatePage should effectively act as if the "Blank" (default) createplate, i.e. a plain <textarea>, was requested.
For reasons that are not quite clear to me, 6cf195214ea2287b30efa60b9a2e12def29a17c2 broke this behavior and instead of the empty <textarea> being displayed, *no* <textarea> whatsoever was displayed (because [[MediaWiki:Createplate-character]] did not exist). We *always* want to display _something_ to ensure that even if a createplate hasn't been configured properly, users are still able to create new pages via Special:CreatePage.

Change-Id: I3e78e02d49162c5122d36e7e908a75ba873dd7a0

includes/CreateMultiPage.php
@@ -54,7 +54,7 @@ class CreateMultiPage {
 		if ( empty( $wgMultiEditTag ) || ( strpos( $sourceText, $multiedit_tag ) === false ) ) {
 			// @phan-suppress-previous-line PhanTypeMismatchArgumentNullableInternal phan doesn't like how $sourceText can (in theory, but not in practise) be empty
 			// @phan-suppress-next-line PhanTypeMismatchArgumentNullableInternal ditto
-			if ( strpos( $sourceText, self::ISBLANK_TAG_SPECIFIC ) === false ) {
+			if ( !strpos( $sourceText, self::ISBLANK_TAG_SPECIFIC ) ) {
 				// @phan-suppress-next-line PhanTypeMismatchArgumentNullableInternal
 				$sourceText = str_replace( self::ISBLANK_TAG_SPECIFIC . "\n", '', $sourceText );
 				$sourceText = str_replace( self::ISBLANK_TAG_SPECIFIC, '', $sourceText );